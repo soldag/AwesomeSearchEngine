9b0e5fe7a6e4db0e9a5397084b8107d3735cd78d
Sören Oldag <soeren_oldag@freenet.de>
2015-10-15 14:02:29 +0200

    Added templates

---
+++ SearchEngine.java
@@ -0,0 +1,63 @@
+package SearchEngine;
+
+/**
+ *
+ * @author: Konstantina.Lazarid
+ * @dataset: US patent grants : ipg files from http://www.google.com/googlebooks/uspto-patents-grants-text.html
+ * @course: Information Retrieval and Web Search, Hasso-Plattner Institut, 2015
+ */
+
+import java.io.File;
+import java.util.ArrayList;
+
+/* The only change you should make in this file is to define your baseDirectory!!
+*  for instance, C:/Users/myuser/Desktop/
+*  In the constructor of your implementation class, namely "public SearchEngineMyTeamName()", you will super the constructor of this abstract class.
+*  Then, as you can see in the "public SearchEngine()", a directory called "SearchEngineMyTeamName" will be created inside the baseDirectory.
+*  This directory is defined in the "teamDirectory" variable.
+*  It will contain all the files that you are using in your search engine.
+*  Namely, all the files that are neccessary for your engine to run (i.e. your stopword list) and all the files that your program generates.
+*/
+public abstract class SearchEngine {
+
+        // paths
+	protected static String baseDirectory = "";  /************* Define your baseDirectory here !! ******************/
+        protected static String teamDirectory; // don't change this
+        
+         // we will need these later in the course
+        protected int topK; 
+        protected int prf; 
+        
+	public SearchEngine() {
+		
+            // the baseDirectory is already defined
+            teamDirectory = baseDirectory + getClass().getSimpleName(); // creates SearchEngineMyTeamName directory
+            new File(teamDirectory).mkdirs();
+	}
+
+        // contruct your patent index and save it in a file in the teamDirectory
+        abstract void index(String directory);
+        
+        // load the index's seeklist from the teamDirectory
+	abstract boolean loadIndex(String directory);
+        
+        // contruct a compressed version of the index and save it in a file in the teamDirectory
+        abstract void compressIndex(String directory);
+
+        // load the seeklist for the compressed index from the teamDirectory
+        abstract boolean loadCompressedIndex(String directory);
+
+        // search the index for a given query and return the relevant patent titles in an ArrayList of Strings
+        abstract ArrayList<String> search(String query, int topK, int prf);
+
+        // we will need this later in the course
+        public void setTopK(int value){
+            topK = value; 
+        }
+        
+        // we will need this later in the course
+        public void setPRF(int value){
+            prf = value;
+        }
+        
+}
---
+++ SearchEngineTest.java
@@ -0,0 +1,39 @@
+package SearchEngine;
+
+/**
+ *
+ * @author: Your team name
+ * @dataset: US patent grants : ipg files from http://www.google.com/googlebooks/uspto-patents-grants-text.html
+ * @course: Information Retrieval and Web Search, Hasso-Plattner Institut, 2015
+ *
+ * You can run your search engine using this file
+ * You can use/change this file during the development of your search engine.
+ * Any changes you make here will be ignored for the final test!
+ */
+
+public class SearchEngineTest {
+    
+    
+    public static void main(String args[]) throws Exception {
+
+        // SearchEngine myEngine = new SearchEngineMyTeamName();
+        
+        // long start = System.currentTimeMillis();
+        
+        // myEngine.index(String directory)
+        
+        // long time = System.currentTimeMillis() - start;
+        
+        // System.out.print("Indexing Time:\t" + time + "\tms\n");
+        
+        // myEngine.loadIndex(String directory)
+        
+        // String query = "";
+        
+        // ArrayList <String> results = new ArrayList <> ();
+        
+        // results = myEngine.search(String query, int topK, int prf)
+        
+    }
+
+}
